a48f148c0b78bd59e4b0abe601c30f056f5966a7
Crowdbotics <team@crowdbotics.com>
2020-05-02 05:06:16 +0000

    Initial commit.

---
+++ dance_of_love_16523/wsgi.py
@@ -0,0 +1,16 @@
+"""
+WSGI config for dance_of_love_16523 project.
+
+It exposes the WSGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/2.2/howto/deployment/wsgi/
+"""
+
+import os
+
+from django.core.wsgi import get_wsgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'dance_of_love_16523.settings')
+
+application = get_wsgi_application()
